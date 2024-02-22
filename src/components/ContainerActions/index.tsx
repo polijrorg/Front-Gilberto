0e7ff5336c21e2cd29d12250b0372ea3ea68ecce
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-02-21 23:50:58 -0300

    Buttons for Managers 🤫

--- src/components/ContainerActions/index.tsx
+++ src/components/ContainerActions/index.tsx
@@ -2,12 +2,30 @@ import * as S from './styles';
 import React from 'react';
 import ButtonWhite from '@components/ButtonWhite';
 import { useNavigation } from '@react-navigation/native';
+import useAuth from '@hooks/useAuth';
 
 const ContainerActions: React.FC = () => {
+  const { user } = useAuth();
   const navigation = useNavigation();
+
+  const buttonConfig = {
+    Gerente: {
+      mentorado: 'Editar Mentoria',
+      visita: 'Editar Visita',
+    },
+    default: {
+      mentorado: 'Avaliar um Mentorado',
+      visita: 'Auditar uma Visita',
+    },
+  };
 
   const handleEnviarMyTeam = () => {
     navigation.navigate('MyTeam' as never);
+  };
+
+  const getButtonText = (buttonType: string) => {
+    const userTypeConfig = buttonConfig[user.job] || buttonConfig.default;
+    return userTypeConfig[buttonType];
   };
 
   return (
@@ -15,9 +33,9 @@ const ContainerActions: React.FC = () => {
       <S.TitleActions>O que você vai fazer hoje?</S.TitleActions>
       <S.DivActions>
         <ButtonWhite text={'Ver Planos de Ação'} />
-        <ButtonWhite text={'Visualizar Equipe'} duty={handleEnviarMyTeam} />
-        <ButtonWhite text={'Avaliar um Mentorado'} />
-        <ButtonWhite text={'Auditar uma Visita'} />
+        <ButtonWhite text="Visualizar Equipe" duty={handleEnviarMyTeam} />
+        <ButtonWhite text={getButtonText('mentorado')} />
+        <ButtonWhite text={getButtonText('visita')} />
       </S.DivActions>
     </S.ContainerActions>
   );
