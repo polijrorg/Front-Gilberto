d482870b01c3684e6147c394ba962aea8cf5aa17
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-06-30 20:51:22 -0300

    Button Realod Grafic

--- src/components/MatrizSlider/index.tsx
+++ src/components/MatrizSlider/index.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View, ScrollView, Dimensions } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import {
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
 import BarChartComponent from '@components/BarChart';
 import * as S from './styles';
 import ModulesServices from '@services/ModuleServices';
 import ModuleGradeServices from '@services/ModuleGradeService';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const MatrizSlider: React.FC = () => {
   const scrollRef = useRef<ScrollView>(null);
   const { width: windowWidth } = Dimensions.get('window');
-  
-  // Ajuste do estado para corresponder ao tipo esperado
-  const [moduleAverages, setModuleAverages] = useState<{ module: string, nameModule: string, average: number }[]>([]);
+  const [moduleAverages, setModuleAverages] = useState<
+    { module: string; nameModule: string; average: number }[]
+  >([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const data = ['modulo', 'competencia', 'matrixxz']; // Tipos de gráficos
+  const data = ['modulo', 'competencia', 'matrixxz'];
 
   const handleScroll = (event) => {
     const offsetX = event.nativeEvent.contentOffset.x;
@@ -23,50 +32,101 @@ const MatrizSlider: React.FC = () => {
   const fetchModuleGradesAverages = async () => {
     try {
       const modulesGrades = await ModuleGradeServices.getGradeModuleAll();
-  
       const modulesWithAverages = await Promise.all(
         modulesGrades.map(async (module) => {
-          const moduleData = await ModulesServices.getModuleById(module.moduleId);
+          const moduleData = await ModulesServices.getModuleById(
+            module.moduleId
+          );
           return {
-            module: module.moduleId, // Renomeando para corresponder ao estado
+            module: module.moduleId,
             nameModule: moduleData.name,
             average: module.average,
           };
         })
       );
-  
       return modulesWithAverages;
     } catch (error) {
       console.error('Erro ao buscar médias dos módulos:', error);
       throw error;
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const handleReload = async () => {
+    setIsLoading(true);
+    const moduleAveragesData = await fetchModuleGradesAverages();
+    setModuleAverages(moduleAveragesData);
+  };
+
   useEffect(() => {
-    const getModuleAverages = async () => {
-      const moduleAveragesData = await fetchModuleGradesAverages();
-      setModuleAverages(moduleAveragesData);
+    const fetchData = async () => {
+      try {
+        const moduleAveragesData = await fetchModuleGradesAverages();
+        setModuleAverages(moduleAveragesData);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
     };
 
-    getModuleAverages();
+    fetchData();
   }, []);
 
   return (
     <S.Wrapper>
-      <ScrollView
-        ref={scrollRef}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        onScroll={handleScroll}
-        scrollEventThrottle={16}
-      >
-        {data.map((type, index) => (
-          <View key={index} style={{ width: windowWidth, borderBottomWidth: 1, borderTopWidth: 1, borderColor: '#d1d1d1' }}>
-            <BarChartComponent type={type} moduleAverages={moduleAverages} />
-          </View>
-        ))}
-      </ScrollView>
+      {isLoading ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <ActivityIndicator size="large" color="#3E63DD" />
+        </View>
+      ) : (
+        <ScrollView
+          ref={scrollRef}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={handleScroll}
+          scrollEventThrottle={16}
+        >
+          {data.map((type, index) => (
+            <View
+              key={index}
+              style={{
+                width: windowWidth,
+                borderBottomWidth: 1,
+                borderTopWidth: 1,
+                borderColor: '#d1d1d1',
+              }}
+            >
+              <BarChartComponent type={type} moduleAverages={moduleAverages} />
+              <TouchableOpacity
+                onPress={handleReload}
+                style={{
+                  position: 'absolute',
+                  top: 10,
+                  right: 10,
+                  backgroundColor: '#fff1',
+                  padding: 8,
+                  borderRadius: 8,
+                }}
+              >
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>
+                  <MaterialCommunityIcons
+                    name="reload"
+                    size={20}
+                    color="#3E63DD"
+                  />
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </S.Wrapper>
   );
 };
